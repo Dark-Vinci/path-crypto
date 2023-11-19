d4c37a5b79269ea0f3b75b6de3eca32cb0d6f296
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-11-19 11:45:47 +0100

    chore: more update

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,10 +5,26 @@ export function Footer(): JSX.Element {
         <div className={ style.container }>
             <div className={ style.header }>
                 <div className={ style.header_wrapper }>
-                    <div className={ style.header_left }></div>
+                    <div className={ style.header_left }>
+                        <div className={ style.logo }>
+                            <img 
+                                src="https://assets-global.website-files.com/62a0c3db69e9f31272bb38a0/6329fe611a33acfb8529f09d_Path-logo.svg" 
+                                alt="" 
+                                loading='lazy'
+                            />
+                        </div>
+
+                        <div className={ style.others }>
+                            <p>300 South Wacker, Suite 1200, Chicago, Illinois</p>
+                            <p>Copyright © 2023 Path Finance Corporation. All rights reserved.</p>
+                        </div>
+                    </div>
+
                     <div className={ style.header_right }>
                         <div className={ style.right_wrapper }>
-                                header
+                            <div className={ style.first }></div>
+                            <div className={ style.second }></div>
+                            <div className={ style.third }></div>
                         </div>
                     </div>
                 </div>
